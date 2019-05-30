c53eefc2ca0dc5b4ffe2ac2837e3138f263d6c26
Chris Lin <chris522229197@gmail.com>
2019-05-30 16:38:32 -0700

    Initial specs for helper functions

---
+++ helpers.R
@@ -0,0 +1,22 @@
+# This script contains helper functions
+
+# Split entire data set into interested treatment group
+
+# Difference of the mean outcome between treated and control group
+# This is the classical way to find ATE in a randomized trial
+
+# ATE with OLS for unconfoundedness assumption
+
+# Calculate propensity score
+
+# IPW to account for propensity
+# Only requires propensity regression
+
+# Plot the calibration of propensity score
+
+# APIW for doubly robust
+# Requires outcome regression and propensity regression
+# Some freedom for what functions/models we use
+
+# For ATE, show the results of different methods. Talk about how they compare to each other.
+# Sensitivity analysis with data set reduction.
